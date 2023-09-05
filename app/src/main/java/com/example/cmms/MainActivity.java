df09b271561823d35eb4a231936bef4668648c35
Akash Maurya <akashmaur94@gmail.com>
2023-09-05 14:07:20 +0300

    Agu LMS App is ready to fly

---
+++ app/src/main/java/com/example/cmms/MainActivity.java
@@ -0,0 +1,267 @@
+package com.example.cmms;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.URLUtil;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    // Making Objects
+    private WebView webView;
+    private String url="https://lms.agu.edu.bh/";
+
+    private static final String KEY_DATA = "data_key";
+    private String dataToSave;
+
+    private String text;
+
+    private ValueCallback<Uri[]> mUploadMessage;
+    private final static int FILECHOOSER_RESULTCODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    // find the object from the xml
+    webView=(WebView) findViewById(R.id.webview);
+
+
+    // Saved Instance
+        if(savedInstanceState !=null){
+            webView.restoreState(savedInstanceState);
+        }
+        else
+        {
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.getSettings().setLoadWithOverviewMode(true);
+            webView.getSettings().setUseWideViewPort(true);
+            webView.getSettings().setDomStorageEnabled(true);
+            webView.getSettings().setLoadsImagesAutomatically(true);
+        }
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); // Enable JavaScript
+        webSettings.setAllowFileAccess(true); // Allow file access
+        webSettings.setAllowContentAccess(true); // Allow content access
+        webSettings.setDomStorageEnabled(true); // Enable DOM storage
+
+
+    //For Full Screen
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        webView.loadUrl(url);
+
+    // set a WebViewClient to handle navigation within the webView
+      webView.setWebViewClient(new WebViewClient(){
+          @Override
+          public boolean shouldOverrideUrlLoading(WebView view, String url) {
+              view.loadUrl(url);
+              return super.shouldOverrideUrlLoading(view, url);
+          }
+      });
+
+        // Dexter Download Plugin
+        webView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(final String s, final String s1, final String s2, final String s3, long l) {
+
+                Dexter.withActivity(MainActivity.this)
+                        .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void onPermissionGranted(PermissionGrantedResponse response) {
+
+
+                                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(s));
+                                request.setMimeType(s3);
+                                String cookies = CookieManager.getInstance().getCookie(s);
+                                request.addRequestHeader("cookie",cookies);
+                                request.addRequestHeader("User-Agent",s1);
+                                request.setDescription("Downloading File.....");
+                                request.setTitle(URLUtil.guessFileName(s,s2,s3));
+                                request.allowScanningByMediaScanner();
+                                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                                request.setDestinationInExternalPublicDir(
+                                        Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(
+                                                s,s2,s3));
+                                DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                                downloadManager.enqueue(request);
+                                Toast.makeText(MainActivity.this, "Downloading File..", Toast.LENGTH_SHORT).show();
+
+                            }
+
+                            @Override
+                            public void onPermissionDenied(PermissionDeniedResponse response) {
+
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                                token.continuePermissionRequest();
+                            }
+                        }).check();
+
+            }
+        });
+
+        // Download the Files from the Website
+        webView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimetype));
+                request.setDescription("Downloading file...");
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimetype));
+                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                dm.enqueue(request);
+                Toast.makeText(getApplicationContext(), "Downloading...", Toast.LENGTH_SHORT).show();
+                registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+            }
+            BroadcastReceiver onComplete = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    Toast.makeText(getApplicationContext(), "Downloading Complete", Toast.LENGTH_SHORT).show();
+                }
+            };
+        });
+
+        // For loading pages and images and videos
+    webView.getSettings().setJavaScriptEnabled(true);
+
+
+        //Upload the file
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient() {
+            public boolean onShowFileChooser(
+                    WebView webView, ValueCallback<Uri[]> filePathCallback,
+                    FileChooserParams fileChooserParams) {
+                if (mUploadMessage != null) {
+                    mUploadMessage.onReceiveValue(null);
+                }
+                mUploadMessage = filePathCallback;
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("*/*"); // MIME type for all files
+                startActivityForResult(intent, FILECHOOSER_RESULTCODE);
+                return true;
+            }
+        });
+
+        webView.loadUrl(url); // Replace with your website URL
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == FILECHOOSER_RESULTCODE) {
+            if (mUploadMessage == null)
+                return;
+            Uri[] result = null;
+            try {
+                if (resultCode == RESULT_OK) {
+                    if (intent != null) {
+                        result = new Uri[]{intent.getData()};
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            mUploadMessage.onReceiveValue(result);
+            mUploadMessage = null;
+        }
+    }
+
+
+
+    // **** closed saved instance *** //
+
+
+
+    //private fuctions for upload the files
+
+    private void checkDownloadPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            Toast.makeText(MainActivity.this, "Write External Storage permission allows us to save files. Please allow this permission in App Settings.", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+        }
+    }
+    // Going Previous Page
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            // Alert Message Box
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Are you sure want to Exit?")
+                    .setNegativeButton("No", null)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            // Finishing Every Activity process
+                            finishAffinity();
+                        }
+                    }).show();
+        }
+
+
+
+}
+    // saved response data while rotating the screen
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        webView.saveState(outState);
+
+    }
+
+
+
+
+
+    // main program closed
+}
